1c668c7a08ce1567fa3705ef0e88761e0bf4f71f
KingLuigi4932 <slmanarendo1950@gmail.com>
2018-02-01 01:49:37 +0300

    Shaper Bootloader 1.0

--- bootloader/bootcode.asm
+++ bootloader/bootcode.asm
@@ -4,7 +4,7 @@ ORG 0x7C00
 jmp short start
 nop
 
-;; [BIOS Parameter Block]
+; [BIOS Parameter Block]
 bpb:
 iOEM db "SHAPEROS"
 iSectSize dw 512 ; bytes / sector
@@ -12,7 +12,7 @@ iClustSize db 1    ; 1 sector per cluster (for simplicitty)
 iResCnt dw 1       ; number of reserved sectors
 iFatCnt db 2       ; # of fat copies
 iRootSize dw 224   ; size of root dir
-iTotalSect dw 2880 ; total sectors
+iTotalSect dw 2880 ; total sectors (size of floppy (in bytes) / bytes per sector)
 iMedia db 0xF0     ; media descriptor
 iFatSize dw 9      ; size of each FAT
 iTrackSect dw 18    ; sectors per track
@@ -25,9 +25,6 @@ iBootSign db 0x29  ; extended bootsig
 iVolID dd 0x50001920           ; disk serial
 acVolumeLabel db "SOS FLOPPY " ; volume label
 acFSType db "FAT16  "          ; fs type
-
-;; [Loader informations]
-fileloader_name db 'LOADER  SSF'
 
 start:
 	mov [iBootDrive], dl
@@ -53,73 +50,175 @@ start:
 	
     mov si, loadmsg
     call puts16
-
-	mov ax, 21h
-	call logicalsector_to_chs
 	
-	jmp jump_to_loader
+	jmp execute_loader
 	
-	; If unexpectdly returned to here then halt the cpu!
-
+; if unexpectdly returned to here then halt the cpu!
 halt:
 	hlt
 	jmp halt
 	
-	intro db 'Shaper Bootloader, written by KingLuigi4932 and Kerndever', 0xD, 0xA, 0
-    loadmsg db 'Loading kernel... ', 0xD, 0xA, 0
-    failed db 'Failed!', 0xD, 0xA, 0
+intro db 'Shaper Bootloader, written by KingLuigi4932 and Kerndever', 0xD, 0xA, 0
+loadmsg db 'Loading kernel... ', 0
+failed_msg db 'Failed!', 0xD, 0xA, 0
+anykey_msg db 'Press any key to reboot!', 0
 
-;; [Floppy functions] -------
+; floppy functions
 bioscall_resetfloppy:
 	mov ah, 00h
 	mov dl, [iBootDrive]
 	int 13h
 	
+	jc puts16_error
+	
 	ret
 	
 jump_to_loader:
-	mov ah, 02h
-	mov al, 01h
 	mov dl, [iBootDrive]
-	mov bx, 0100h
-	mov es, bx
-	mov bx, 00h
-	int 13h
-	
-	jc print_error
-	
-	mov dl, [iBootDrive]
-	jmp 1000h
+	mov byte [9C04h], dl
+	jmp 9C05h
 	
 	ret
 	
-;; [Utils] -----------------------
-
-;; Special Thanks to Mike Saunders and MikeOS Developers
-;;; LBA to CHS Conversion
-;;; INPUT: AX = LBA | OUTPUT: CH = CYLINDER, DH = HEAD, CL = SECTOR
-logicalsector_to_chs:
 	
-	mov bx, ax			; Save logical sector
+execute_loader:
+	call load_rootdir
+	call get_file_loader
+	call load_file_loader_sectors
+	call jump_to_loader
+	
+	ret
+	
+load_rootdir:
+	mov ah, 02h
+	mov al, 0Eh
+	mov ch, 00h
+	mov cl, 02h ; |
+	mov dh, 01h ; | sector 0x13 (root dir sector)
+	mov dl, [iBootDrive]
+	mov bx, 0A00h
+	mov es, bx
+	mov bx, 0000h
+	int 13h
+	
+	jc puts16_error
+	
+	ret
+	;jmp $
+	
+get_file_loader:
+	;push ax
+	mov di, 0A000h
+	mov si, loader_fat32_filename
+	
+.loop:
+	; check if reached it limits
+   cmp di, (0xA000 + (0x20 * 224))
+   je puts16_error
 
-	mov dx, 0			; First the sector
+   mov al, [si]
+   mov bl, [di]
+   cmp al, bl
+   jne .next
+   
+   cmp al, 0
+   je .found
+  
+   inc di
+   inc si
+   jmp .loop ; loop!
+   
+   .next:
+	add di, 20h
+	jmp .loop
+	
+   .found:
+	mov bp, di
+	sub bp, 2Bh
+	
+	; little endian assignment
+	 ; starting cluster
+	mov cl, [bp+26]
+	mov ch, [bp+27]
+	 ; size
+	mov dl, [bp+28]
+	mov dh, [bp+29]
+	
+	mov [loader_starting_cluster], cx 
+	mov [loader_file_size], dx
+	
+	;jmp $
+	
+	;pop cx 
+	;pop dx
+	
+	 ; amount of clusters
+	mov ax, dx
+	xor dx, dx
+	mov cx, 200h
+	div cx
+	inc ax ; increment one cluster because (floating numbers even if they are greater than (x.4) still the reuslt is (x))
+	
+	mov [loader_clusters], ax
+	
+	ret
+	
+load_file_loader_sectors:
+	mov ax, 1Fh
+	add ax, word [loader_starting_cluster]
+	call ls2chs
+
+	mov ah, 02h
+	mov al, byte [loader_clusters]
+	mov bx, 09C0h
+	mov es, bx
+	mov bx, 0
+	mov dl, [iBootDrive]
+	int 13h
+	
+	jc puts16_error
+	
+	ret ;jump_to_loader
+	
+; utils
+; credits: mike saunders and mikeos developers
+ls2chs:
+	
+	mov bx, ax			; save logical sector
+
+	mov dx, 0			; first the sector
 	div word [iTrackSect]
-	add dl, 01h			; Physical sectors start at 1
-	mov cl, dl			; Sectors belong in CL for int 13h
+	add dl, 01h			; physical sectors start at 1
+	mov cl, dl			; sectors belong in CL for int 13h
 	mov ax, bx
 
 	mov dx, 0			; Now calculate the head
 	div word [iTrackSect]
 	mov dx, 0
 	div word [iHeadCnt]
-	mov dh, dl			; Head/side
-	mov ch, al			; Track
+	mov dh, dl			; head/side
+	mov ch, al			; track
 
-	mov dl, byte [iBootDrive]		; Set correct device
+	mov dl, byte [iBootDrive]		; set correct device
 
 	ret
 	
-;; [Print functions] -------------
+; print functions
+puts16_error:
+	mov si, failed_msg
+	call puts16
+	
+	mov si, anykey_msg
+	call puts16
+	
+	mov ah, 00h
+	int 16h
+	
+	; jmp word 0xFFFF:0x0000
+    db 0xEA 
+    dw 0x0000 
+    dw 0xFFFF
+
 puts16:
 	mov ah, 0Eh		
 
@@ -132,21 +231,16 @@ puts16:
 
 .done:
 	ret
-	
-print_error:
-    mov si, failed
-    call puts16
-	
-	jmp $
 
 clear_screen:
-	; Clear screen
-	;; Set mode to clear screen for all bioses
+	; clear screen
+	
+	 ; set mode to clear screen for all bioses
 	mov AH, 00h
 	mov AL, 02h ; AL=02 (80x25 text mode)
-	int 10h ; BIOS style: Video calls
+	int 10h
 	
-	;; Fill screen with blue background
+	 ; fill screen with blue background
 	mov AH, 09h
 	mov AL, 20h
 	mov BH, 00h
@@ -155,8 +249,12 @@ clear_screen:
 	int 10h
 	
 	ret
+	
+loader_fat32_filename db 'LOADER  BIN'
+loader_starting_cluster dw 0
+loader_file_size dw 0
+loader_clusters db 0
 
+; boot signature
 times 510-($-$$) db 0
 dw 0xAA55
-
-diskbuffer:
